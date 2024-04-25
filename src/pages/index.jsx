a20c30e8b50350f8255cc4022af2b5a0da2c6daa
wanye6688@gmail.com <wanye6688@gmail.com>
2024-04-25 11:09:01 +0800

    revise

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,107 +16,6 @@ import ProductOrdersOrderCard from 'src/components/gridcard/counter-card/product
 
 // import ApplicationUiTablesOrdersList from 'src/application-ui/tables/orders-list/orders-list';
 
-const employeesTest = [
-  {
-    name: '環景影像顯示系統(ECU+線束+配件包)',
-    jobFunction: 'UI Engineer, Apple Inc.',
-    avatar: '/avatars/1.png',
-    status: 'Pending',
-    statusColor: 'warning',
-  },
-  {
-    name: '環景影像顯示系統(四鏡頭組)',
-    jobFunction: 'Frontend Developer',
-    avatar: '/avatars/2.png',
-    status: '噗噗中',
-    statusColor: 'success',
-  },
-  {
-    name: 'PVM錄影盒 (適用尊爵旗艦GR版)',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-  {
-    name: 'Regan Norris123',
-    jobFunction: 'Senior Project Manager',
-    avatar: '/avatars/3.png',
-    status: 'Declined',
-    statusColor: 'error',
-  },
-];
-
 
 const dropdownCarItems = [
   {
@@ -131,9 +30,9 @@ const dropdownCarItems = [
   },
   {
     itemName: '車型',
-    itemLists:[],
-    itemListsToyota: ['Rav4','COROLLA CROSS'],
-    itemListsLEXUS: ['RX200','UX200'],
+    itemLists: [],
+    itemListsToyota: ['Rav4', 'COROLLA CROSS'],
+    itemListsLEXUS: ['RX200', 'UX200'],
   },
   {
     itemName: '仕向地',
@@ -179,11 +78,11 @@ const Page = () => {
   // const [employees,setEmployees] = useState({});
   const [isCarOrderTablesListPageOpen, setIsCarOrderTablesListPageOpen] = useState(0);
   const isMobile = useMediaQuery('(max-width:780px)');
-  const [carSearchState,setCarSearchState] = useState(dropdownCarItems);
+  const [carSearchState, setCarSearchState] = useState(dropdownCarItems);
 
   const handleTabChange = (tabIndex) => {
     setIsCarOrderTablesListPageOpen(tabIndex);
-    console.log('tabIndex:',tabIndex)
+    console.log('tabIndex:', tabIndex)
   };
 
 
@@ -191,13 +90,15 @@ const Page = () => {
   return (
     <>
       <Helmet heading="Homepage" />
+
       <Box
         component="main"
         flex={1}
         display="flex"
         flexDirection="column"
-        py={{xs: 2,sm:3 }}
+        py={{ xs: 2, sm: 3 }}
         sx={{
+
           backgroundImage: (theme) =>
             `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('/placeholders/covers/login.jpg')`,
           backgroundSize: 'cover',
@@ -207,98 +108,23 @@ const Page = () => {
           },
         }}
       >
-        
-        
+
+        <Box
+         sx={{
+          backgroundColor: 'red',
+         }}
+        >
+
+
+        {/*========================Main=======================*/}
+
+
+
+        <ApplicationUiHorizontalMenusTabs onTabChange={handleTabChange} />
+        </Box>
+
+
         <Container disableGutters maxWidth="xl">
-          <PageHeading
-            sx={{
-              px: {
-                xs: 2,
-                sm: 3,
-              },
-              pb: {
-                xs: 2,
-                sm: 3,
-              },
-            }}
-            // title="Homepage"
-            // description="This is the Home page for the starter template"
-            actions={
-              <>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              {/* <Button
-                  sx={{
-                    mt: {
-                      xs: 2,
-                      md: 0,
-                    },
-                    padding: '10px 20px'
-                  }}
-                  variant="contained"
-                  startIcon={<FileDownloadOutlinedIcon fontSize="small" />}
-                  // onClick={TestClearEmepolyFunction}
-                >
-                  {t('Export')}
-                </Button> */}
-                {/* <ButtonSoft 
-                 variant="contained"
-                >
-                  clear
-                </ButtonSoft> */}
-              </Box>
-                
-              </>
-            }
-            // iconBox={
-            //   <AvatarState
-            //     isSoft
-            //     variant="rounded"
-            //     state="primary"
-            //     sx={{
-            //       height: theme.spacing(7),
-            //       width: theme.spacing(7),
-            //       svg: {
-            //         height: theme.spacing(4),
-            //         width: theme.spacing(4),
-            //         minWidth: theme.spacing(4),
-            //       },
-            //     }}
-            //   >
-            //     <ChartBarIcon />
-            //   </AvatarState>
-            // }
-          />
-          
-          {/* <Divider /> */}
-
-
-
-          {/*======================= Function ======================= */}
-          {/* <Box
-            px={{
-              xs: 2,
-              sm: 3,
-            }}
-            pt={{
-              xs: 2,
-              sm: 3,
-            }}
-            
-          >
-            <Typography
-              gutterBottom
-              variant="h4"
-            >
-              This is the Home page for the Carmax Merchandise
-            </Typography>
-            <Typography variant="subtitle1">
-
-            </Typography>
-          
-          </Box> */}
-
-
-          {/*========================Main=======================*/}
           <Box
             px={{
               xs: 1,
@@ -309,18 +135,15 @@ const Page = () => {
               sm: 1,
             }}
 
-           
           >
-          <ApplicationUiHorizontalMenusTabs onTabChange  = {handleTabChange} />
-           
-          
-          {isCarOrderTablesListPageOpen ===0  &&<CarOrderCard dropdownCarItems={dropdownCarItems}/>}
-          
-          {isCarOrderTablesListPageOpen ===1  &&<ProductOrdersOrderCard dropdownProductItems={dropdownProductItems}/>}
-          {/* {isCarOrderTablesListPageOpen ===0  && <CarOrderTablesListPage dropdownCarItems={dropdownCarItems}/>} */}
-          {/* {isCarOrderTablesListPageOpen ===1  && <ProductOrdersTable dropdownProductItems={dropdownProductItems}/>} */}
+            {isCarOrderTablesListPageOpen === 0 && <CarOrderCard dropdownCarItems={dropdownCarItems} />}
+
+            {isCarOrderTablesListPageOpen === 1 && <ProductOrdersOrderCard dropdownProductItems={dropdownProductItems} />}
+            {/* {isCarOrderTablesListPageOpen ===0  && <CarOrderTablesListPage dropdownCarItems={dropdownCarItems}/>} */}
+            {/* {isCarOrderTablesListPageOpen ===1  && <ProductOrdersTable dropdownProductItems={dropdownProductItems}/>} */}
 
           </Box>
+
         </Container>
       </Box>
     </>
